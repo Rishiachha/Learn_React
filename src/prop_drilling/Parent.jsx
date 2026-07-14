1e074b3fcdda0cb76c07075fd2bd44e21ae67f2e
Achha Srishanth rishi <srishanthrishi9@gmail.com>
2026-07-14 13:16:55 +0530

    Document props drilling concept in Parent.jsx
    Added explanation for props drilling in Parent component.

--- src/prop_drilling/Parent.jsx
+++ src/prop_drilling/Parent.jsx
@@ -9,7 +9,7 @@ export const Parent = ({name}) => {
         </div>
     )
 }
-// Props Drilling
+// Props Drilling : Props Drilling means passing data through multiple components, even if the middle component doesn't use the data.
 // App
 // ↓
 // Parent
